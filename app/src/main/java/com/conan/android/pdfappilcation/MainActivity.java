614cf7791d44ec6b8c7d28aa933a49fcc6f381a8
tconan <62794232@qq.com>
2018-05-21 10:57:32 +0800

    添加功能支持

--- app/src/main/java/com/conan/android/pdfappilcation/MainActivity.java
+++ app/src/main/java/com/conan/android/pdfappilcation/MainActivity.java
@@ -13,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.button).setOnClickListener(v -> {
+            X5WebViewActivity.path = "http://192.168.1.177:5050/";
+            Intent intent = new Intent(MainActivity.this,
+                    X5WebViewActivity.class);
+            startActivity(intent);
+        });
+
+        findViewById(R.id.button2).setOnClickListener(v -> {
+            X5WebViewActivity.path = "http://192.168.1.177:7080/";
             Intent intent = new Intent(MainActivity.this,
                     X5WebViewActivity.class);
             startActivity(intent);
--- app/src/main/java/com/conan/android/pdfappilcation/X5WebViewActivity.java
+++ app/src/main/java/com/conan/android/pdfappilcation/X5WebViewActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
 
 public class X5WebViewActivity extends AppCompatActivity {
 
     private WebView webView;
+
+    public static String path = "http://192.168.1.239:5050/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,23 @@ public class X5WebViewActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.web_view);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://192.168.1.239:5050/");
+
+        // 添加本地存储支持
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAppCacheMaxSize(1024 * 1024 * 8);
+        webView.getSettings().setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAppCacheEnabled(true);
+
+        // 支持地址拦截
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                webView.loadUrl(url);
+                return super.shouldOverrideUrlLoading(webView, url);
+            }
+        });
+
+        webView.loadUrl(path);
     }
 }
